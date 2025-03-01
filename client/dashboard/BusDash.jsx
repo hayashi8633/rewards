48b51ad7942ffba98381547fbd2482f7e87a3b88
Chapman <chapman.chappelle@gmail.com>
2025-03-01 15:28:42 -0500

    finished functionality pre backend (i think)

---
+++ client/dashboard/BusDash.jsx
@@ -0,0 +1,88 @@
+//imports
+import React, { useState } from 'react';
+import BusHeader from './BusHeader';
+import BusAddCustomer from './BusAddCustomer';
+import BusCustomerList from './BusCustomerList';
+import BusManageRewards from './BusManageRewards';
+
+const BusDash = () => {
+  //test customer list
+  const testCustomerList = [
+    {
+      id: 1,
+      name: 'Chapman',
+      phone: 111,
+      num_of_visits: 1,
+    },
+    {
+      id: 2,
+      name: 'Yihe',
+      phone: 222,
+      num_of_visits: 2,
+    },
+    {
+      id: 3,
+      name: 'Rachel',
+      phone: 333,
+      num_of_visits: 3,
+    },
+    {
+      id: 4,
+      name: 'Vicky',
+      phone: 444,
+      num_of_visits: 4,
+    },
+    {
+      id: 5,
+      name: 'Katherine',
+      phone: 555,
+      num_of_visits: 5,
+    },
+  ];
+  //local states
+  //ok
+  const [companyName] = useState('companyName');
+  const [newCustomer, setNewCustomer] = useState({ phone: '' });
+  const [customers, setCustomers] = useState(testCustomerList);
+
+  const addCustomer = () => {
+    if (!newCustomer.phone.trim()) return;
+
+    const customer = {
+      id: 999,
+      name: newCustomer.phone,
+      num_of_visits: 0,
+    };
+    setCustomers([...customers, customer]);
+    setNewCustomer({ phone: '' });
+  };
+
+  const updateVisits = (id, addend) => {
+    setCustomers(
+      customers.map((customer) => {
+        if (customer.id === id) {
+          return {
+            ...customer,
+            num_of_visits: customer.num_of_visits + addend,
+          };
+        }
+        return customer;
+      })
+    );
+  };
+
+  return (
+    <div className='dashboard' style={{ padding: '20px' }}>
+      <BusHeader companyName={companyName} />
+      <BusAddCustomer
+        newCustomer={newCustomer}
+        setNewCustomer={setNewCustomer}
+        addCustomer={addCustomer}
+      />
+      <BusCustomerList customers={customers} updateVisits={updateVisits} />
+      <BusManageRewards />
+    </div>
+  );
+};
+
+export default BusDash;
