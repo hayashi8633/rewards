1babfc92323cd60edafcbe42fdff6ff7bf46456c
katpburke <katherinepackertburke@gmail.com>
2025-03-04 16:48:23 -0600

    updated bus and customer controller logic

--- server/controllers/busController.js
+++ server/controllers/busController.js
@@ -1,18 +1,15 @@
 // Import the users object
 // import db from "../models/models.js"
-import { pool } from "../models/models.js";
-import { supabase } from "../server.js";
+import { pool } from '../models/models.js';
+import { supabase } from '../server.js';
 
 const busController = {};
 
 // TODO NOTE FOR RACHEL: REMEMBER THE REQ.HEADERS THING (put in handleClick header "user": "username" and you can have access to it and pass it as a param to the backend)
 
-// git push -u businessDashboard
-// then approve/deny pull request
-
 busController.addCustomer = async (req, res, next) => {
   try {
-    console.log('🚀 add customer middleware reached!')
+    console.log('🚀 add customer middleware reached!');
     console.log('Request body: ', req.body);
     // console.log('request query:', req.query);
     //logic for adding a customer to the rewards program
@@ -21,12 +18,8 @@ busController.addCustomer = async (req, res, next) => {
     // not exists, add the information of the customer to database
     // phone number, name, number of visits
 
-    const correctBusinessMaybe = req.body.business_name
+    const data = [req.body.business_name, req.body.phone, req.body.name];
 
-    // const business = req.cookies;
-    // console.log('req.cookies', business)
-    const data = [correctBusinessMaybe, req.body.phone, req.body.name];
-    console.log('data we are inserting: ', data);
     // this query will add ONLY existing users from the "accounts" table
     // it'll get the phone number and name of the person from the front end and go through all the data in the "accounts" table- if a person with a matching name and phone # come up, it'll add their name, phone number, and number of visits (automatically set to 0) to the "business_info" table
     const addingCustomer = `INSERT INTO business_info (business_name, phone, num_of_visits, customer_name) 
@@ -36,15 +29,13 @@ busController.addCustomer = async (req, res, next) => {
         WHERE phone = $2 
       );`;
     const result = await pool.query(addingCustomer, data);
-    // console.log("result: ", result);
-    // res.locals.newCustomer = result.rows[0];
     return next();
   } catch (err) {
     // error handling
-    console.log("error: " , err);
+    console.log('error: ', err);
     return next({
-      log: "log: Error adding customer",
-      message: "Message: Error adding customer",
+      log: 'log: Error adding customer',
+      message: 'Message: Error adding customer',
     });
   }
 };
@@ -56,48 +47,38 @@ busController.addStar = async (req, res, next) => {
     // if exists add number of visits
     // if not, add Customer
 
-    // req.body.incOrDec = req.body.increaseOrDecrease
-    // the buttons should either be returning +1 or -1
     // req.body.phone is included in data because each component that is shown on the business dashboard should have a phone number associated with it (not just displayed) -> if i console.log(phone) in the onclick, the phone number of that specific user should be console logged
-    // const data = [res.locals.user, req.body.incOrDec, req.body.phone];
     // We're getting the user from res.body
     const data = [req.body.business_name, req.body.amount, req.body.phone];
-    console.log("Data from POST request in addStar: ", req.body);
+    console.log('Data from POST request in addStar: ', req.body);
 
-    // TODO tell chapman that the button that increases/decreases the number of visits has to be a value of "1" or "-1" each time
     const addingStar =
-      "UPDATE business_info SET num_of_visits=num_of_visits + $2 WHERE business_name=$1 AND phone=$3";
+      'UPDATE business_info SET num_of_visits=num_of_visits + $2 WHERE business_name=$1 AND phone=$3';
     const result = await pool.query(addingStar, data);
-    res.locals.addedStar = result.rows;
+    // res.locals.addedStar = result.rows;
     // console.log('result.rows from addStar middleware: ', result)
     return next();
   } catch (err) {
     // error handling
     return next({
-      log: "log: Error adding a star",
-      message: "Message: Error adding a star",
+      log: 'log: Error adding a star',
+      message: 'Message: Error adding a star',
     });
   }
 };
 
 // This middleware will be used to login the customer
 busController.getDash = async (req, res, next) => {
-  console.log("Business DASHBOARD middleware reached");
+  console.log('Business DASHBOARD middleware reached');
   try {
     //logic for business dashboard getting(?)
     // get names of all rewards members, the number of times they've visited, customer's phone number and the customer's name
     // display it (aka feed it to the front end):D
 
-    // getting the user
-    // TODO check if this even works or if i have to use a req.body/req.param somewhere
-    // const data = [res.locals.user];
-    // const data = ["chipotle"];
-
     // Get business name from req.query
     const data = [req.query.businessName];
 
     console.log('data from busController.getDash: ', data);
-    // I THINK THIS WILL BE A TABLE OF ALL GIVEN CUSTOMERS FOR ONE BUSINESS
     // TODO i think the "coalesce(b.num_of_visits, 0)" could become an issue cuz it changes NULL values to 0 automatically but our table doesn't have NULLs, and because we kinda want the database to reset to 0 after it hits 10
     // might also have to get rid of the quotes around ${data}
     const busDash = `SELECT a.name AS customer_name, 
@@ -112,26 +93,23 @@ busController.getDash = async (req, res, next) => {
       ORDER BY id DESC`;
 
     // const result = await pool.query(busDash);
-    const queryResult = await pool.query(busDash) // Renamed result -> queryResult because it's more descriptive and harder to confuse with the response we'd send back
-    
+    const queryResult = await pool.query(busDash); // Renamed result -> queryResult because it's more descriptive and harder to confuse with the response we'd send back
+
     // Transform response for front end
-    const response = queryResult.rows.map(row => ({
+    const response = queryResult.rows.map((row) => ({
       id: row.id,
       name: row.customer_name,
       phone: row.customer_phone,
-      num_of_visits: row.num_of_visits
+      num_of_visits: row.num_of_visits,
     }));
-    
-    console.log("buscontroller.getDash queryResult: ", queryResult);
-    console.log("buscontroller.getDash responses: ", response);
     // res.locals.allCustomers = result.rows;
     res.locals.allCustomers = response;
     return next();
   } catch (err) {
     // error handling
     return next({
-      log: "log: Error getting the business dashboard",
-      message: "Message: Error getting the business dashboard",
+      log: 'log: Error getting the business dashboard',
+      message: 'Message: Error getting the business dashboard',
     });
   }
 };
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,7 +1,7 @@
 // Import the users object
 // import db from "../models/models.js"
-import { pool } from "../models/models.js";
-import { supabase } from "../server.js";
+import { pool } from '../models/models.js';
+import { supabase } from '../server.js';
 
 const userController = {};
 
@@ -9,11 +9,11 @@ const userController = {};
 
 // This middleware will be used to login the user (could be customer or business)
 userController.loginUser = (req, res, next) => {
-  console.log("Customer login middleware reached");
+  console.log('Customer login middleware reached');
   const data = [req.body.phone, req.body.password];
   // res.locals.currentUser = req.body;
   const existingCust =
-    "SELECT name, user_type FROM accounts WHERE phone=$1 AND password=$2";
+    'SELECT name, user_type FROM accounts WHERE phone=$1 AND password=$2';
   // automatically assumes the login wll not work
   res.locals.loginSuccessful = false;
   try {
@@ -21,66 +21,38 @@ userController.loginUser = (req, res, next) => {
 
     // check if phone # exists first
     pool.query(existingCust, data, (error, results) => {
-      console.log("results", results.rows);
-      res.locals.currentUser = {username: results.rows[0].name, phone: req.body.phone};
+      console.log('results', results.rows);
+      res.locals.currentUser = {
+        username: results.rows[0].name,
+        phone: req.body.phone,
+      };
       // ^ this prints [ { name: 'rachel' } ]
 
       if (results.rowCount === 1) {
         // user is now logged in
         res.locals.loginSuccessful = true;
         // user's name is saved (for the "welcome, [user] message")
-        res.locals.user = {username: results.rows[0].name, usertype: results.rows[0].user_type.toLowerCase()};
+        res.locals.user = {
+          username: results.rows[0].name,
+          usertype: results.rows[0].user_type.toLowerCase(),
+          phone: req.body.phone,
+        };
         return next();
       } else if (results.rowCount === 0) {
         return next({
-          log: "log: No user found",
-          message: "Message: No user found",
+          log: 'log: No user found',
+          message: 'Message: No user found',
         });
       }
     });
-    // res.redirect(`http://localhost:5173/business`)
-    // check whether they're a business or customer (userType column in the database)
-    // check if the password matches the phone # given
   } catch (err) {
     // error handling
     return next({
-      log: "log: Error getting users",
-      message: "Message: Error getting users",
+      log: 'log: Error getting users',
+      message: 'Message: Error getting users',
     });
   }
 };
-
-// Rachel's version
-// userController.register = async (req, res, next) => {
-//   // get the name (of the business/customer), phone #, and password from the frontend
-//   // put "customer" in database if the user didn't click "yes" to the "are you a business?" question
-//   // put "business" in database if they did
-//   const { name, phoneNum, password, userType } = req.body;
-//   try {
-//     //logic for registration attempts
-//     // check if the phone # already exists
-//     // const text = `SELECT EXISTS (SELECT 1 FROM accounts WHERE phone = $1) AS exists;`;
-//     // ^ was here before
-//     const { user_type, phone, name } = req.body;
-
-//     // user_type should pass 'customer' into the database if the "are you a business?" box is not checked- if it is, it should pass 'business' so that the SQL database can be updated accordingly
-//     const text = `INSERT INTO accounts (user_type, phone, name) values ($1, $2, $3)`;
-//     // return { "exists": true } if exists
-//     const result = await pool.query(text, [user_type, phone, name]);
-//     res.locals.user = result;
-//     return next();
-//     // if (result.rows[0].exists)
-//     // if it does, return an error (or yell at the person registering and call em a dummy)
-//     // if it doesn't, log them in! and add a new row in business_name Table
-//     // MINI STRETCH GOAL: if the phone # already exists, instead of throwing an error and breaking code, the frontend could shake and the input fields could turn red and a little message could show up at the bottom of the screen saying "this phone # is already taken!"
-//   } catch (err) {
-//     // error handling
-//     return next({
-//       log: "log: Error registering user",
-//       message: "Message: Error registering user",
-//     });
-//   }
-// };
 
 // Yihe's version
 userController.register = async (req, res, next) => {
@@ -88,27 +60,34 @@ userController.register = async (req, res, next) => {
   // put "customer" in database if the user didn't click "yes" to the "are you a business?" question
   // put "business" in database if they did
   const { username, phone, password, usertype } = req.body;
-  res.locals.currentUser = req.body;
+  // res.locals.currentUser = req.body;
   // console.log(req.body.username)
   try {
     //logic for registration attempts
     // check if the phone # already exists
-    console.log('TRY BLOCK ENTERED ALKDJFALSKDJF')
+    console.log('TRY BLOCK ENTERED ALKDJFALSKDJF');
     const text = `SELECT EXISTS (SELECT 1 FROM accounts WHERE phone = $1) AS exists;`;
     // return { "exists": true } if exists
     const result = await pool.query(text, [phone]);
     if (result.rows[0].exists) {
       return next({
-        log: 'Phone number existed.',
+        log: 'Phone number existed already.',
         status: 400,
         message: { err: 'The phone number is registered.' },
       });
     }
     const text1 = `INSERT INTO accounts (name, phone, password, user_type) VALUES ($1, $2, $3, $4)`;
-    const results1 = await pool.query(text1, [username, phone, password, usertype]);
-    // res.locals.user = results1;
-    res.locals.user = {username: username, usertype: usertype.toLowerCase()};
-    // res.status(201).json({ message: 'Registration successful!' });
+    const results1 = await pool.query(text1, [
+      username,
+      phone,
+      password,
+      usertype,
+    ]);
+    res.locals.user = {
+      username: username,
+      usertype: usertype.toLowerCase(),
+      phone: phone,
+    };
     return next();
     // if it does, return an error (or yell at the person registering and call em a dummy)
     // if it doesn't, log them in! and add a new row in business_name Table
@@ -125,28 +104,37 @@ userController.register = async (req, res, next) => {
 };
 
 // THIS IS THE ONLY MIDDLEWARE THAT IS *ONLY* FOR CUSTOMERS
-userController.getDash = (req, res, next) => {
+userController.getDash = async (req, res, next) => {
   try {
     //logic for customer dashboard getting(?)
-    const data = [req.cookies.phone];
-    console.log("req.body:", req.body)
+
+    const customerName = [req.query.customerName];
+    //use req.query.customerName to get phone number
+    //then pass that into data
+    const phoneQuery = `SELECT phone FROM accounts WHERE name=$1;`;
+    const results = await pool.query(phoneQuery, customerName);
+    const data = [results.rows[0].phone];
+
+    // const data = [req.cookies.phone];
+    // console.log('req.body:', req.body);
+
     const custDash = `SELECT * FROM (SELECT b.business_name, b.num_of_visits, b.phone, b.customer_name 
         FROM business_info b 
            RIGHT JOIN accounts a ON b.phone = a.phone) AS U WHERE U.phone = $1;`;
 
     pool.query(custDash, data, (error, results) => {
       res.locals.dashboard = results.rows;
-      console.log(res.locals.dashboard)
+      console.log(res.locals.dashboard);
       return next();
-    })
-    
+    });
+
     // get names of all the businesses they're a rewards member at, the number of times they've visited each business, and the customer's name
     // display it (aka feed it to the front end):D
   } catch (err) {
     // error handling
     return next({
-      log: "log: Error getting customer dashboard",
-      message: "Message: Error getting customer dashboard",
+      log: 'log: Error getting customer dashboard',
+      message: 'Message: Error getting customer dashboard',
     });
   }
 };
@@ -155,21 +143,35 @@ userController.isLoggedIn = (req, res, next) => {
   const { phone, username } = req.cookies;
   console.log('phone, ', phone);
   console.log('username, ', username);
+  const endpoint = req.query.customerName;
+
+  console.log('endpoint: ', endpoint);
 
   //SQL query to make sure that the ID and username match an ID and username in the database
   const text = 'SELECT * FROM accounts WHERE phone=$1 AND name=$2';
   const values = [phone, username];
   pool.query(text, values).then((response) => {
-    if (response) return next();
-    else {
-      res.redirect('/');
+    // console.log('here is what isLogged in found: ', response);
+    if (
+      response.rows[0].name === username &&
+      response.rows[0].phone === phone &&
+      username === endpoint
+    ) {
+      res.locals.loggedIn = true;
+      return next();
+    } else {
+      console.log('you tried to go to the wrong page!');
+      res.locals.loggedIn = false;
+      return next();
+      // res.redirect('http://localhost:5173/');
+      //this does not work!
     }
   });
 };
 
 userController.setCookie = (req, res, next) => {
   try {
-    const currentUser = res.locals.currentUser;
+    const currentUser = res.locals.user;
     res.cookie('phone', currentUser.phone);
     res.cookie('username', currentUser.username);
     // console.log('res.locals.user: ', res.locals.user);
